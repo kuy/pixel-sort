f8f4748d9b7c8afcd47a018632689e42da305fa8
Yuki Kodama <endflow.net@gmail.com>
2019-01-29 20:37:02 +0900

    First commit

---
+++ sort.py
@@ -0,0 +1,29 @@
+# Copyright(c) 2019 Yuki KODAMA / @kuy
+# This script is distributed under the MIT License.
+
+from PIL import Image
+
+orig = Image.open("image.jpg")
+orig.show()
+
+def takeComp(i):
+    def take(c):
+        return c[i]
+    return take
+
+for c in range(3):
+    gen = Image.new(orig.mode, orig.size)
+
+    xmax, ymax = orig.size
+    for x in range(xmax):
+        buf = []
+        for y in range(ymax):
+            pixel = orig.getpixel((x, y))
+            buf.append(pixel)
+        
+        buf.sort(key=takeComp(c), reverse=True)
+
+        for y in range(ymax):
+            gen.putpixel((x, y), buf[y])
+
+    gen.show()
